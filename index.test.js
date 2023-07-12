89d76992d945b3c23e5219518921b6ecbe81bd78
Socheat Sok <socheatsok78@gmail.com>
2023-07-12 23:43:04 +0700

    Update unit test

--- index.test.js
+++ index.test.js
@@ -3,8 +3,41 @@ const cp = require('child_process');
 const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
+test('test query=nginx', () => {
   process.env['INPUT_QUERY'] = 'nginx';
+  const ip = path.join(__dirname, 'index.js');
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  console.log(result);
+})
+
+test('test query=nginx, limit=10', () => {
+  process.env['INPUT_QUERY'] = 'nginx';
+  process.env['INPUT_LIMIT'] = 10;
+  const ip = path.join(__dirname, 'index.js');
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  console.log(result);
+})
+
+test('test query=nginx, date=2022', () => {
+  process.env['INPUT_QUERY'] = 'nginx';
+  process.env['INPUT_DATE'] = "2022";
+  const ip = path.join(__dirname, 'index.js');
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  console.log(result);
+})
+
+test('test query=nginx, version=1.22', () => {
+  process.env['INPUT_QUERY'] = 'nginx';
+  process.env['INPUT_VERSION'] = "1.22";
+  const ip = path.join(__dirname, 'index.js');
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  console.log(result);
+})
+
+test('test query=nginx, date=2022, version=1.22', () => {
+  process.env['INPUT_QUERY'] = 'nginx';
+  process.env['INPUT_DATE'] = "2022";
+  process.env['INPUT_VERSION'] = "1.22";
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
