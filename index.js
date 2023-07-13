f36e8f5ea1b41d70b59ff5e9bf6dc57591140691
Socheat Sok <socheatsok78@gmail.com>
2023-07-13 09:03:56 +0700

    Add verbose logs

--- index.js
+++ index.js
@@ -19,37 +19,40 @@ function jsonToMatrix(json) {
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const action = {
+    const inputs = {
       search: core.getInput('search'),
       date: core.getInput('date'),
       version: core.getInput('version'),
       limit: core.getInput('limit'),
     }
 
-    if (action.search == "") {
+    if (inputs.search == "") {
       core.setFailed("The search input is required.")
       return
     }
 
-    const data = await getReleaseData(action.search)
+    const data = await getReleaseData(inputs.search)
 
     // The data is a JSON object that the key is the version and the value is the release date
     // filter data for the version that are release in 2022
     let releases = Object.entries(data)
       .filter(([, release_date]) => {
-        if (!action.date) return true
-        return new Date(release_date) >= new Date(action.date)
+        if (!inputs.date) return true
+        core.info(`Filter releases by date: ${inputs.date}`)
+        return new Date(release_date) >= new Date(inputs.date)
       })
       .filter(([ver]) => {
-        if (!action.version) return true
-        return ver.startsWith(action.version)
+        if (!inputs.version) return true
+        core.info(`Filter releases by version: ${inputs.version}`)
+        return ver.startsWith(inputs.version)
       })
       .sort(([a], [b]) => compareVersions(a, b))
 
-    if (action.limit === 0) {
+    if (inputs.limit === 0) {
       core.setFailed("The limit input cannot be zero.")
       return
-    } else if (action.limit > 0) {
+    } else if (inputs.limit > 0) {
+      core.info(`Limit releases by: ${inputs.limit}`)
       releases = releases.reverse().splice(0, 5).reverse()
     }
 
@@ -60,7 +63,7 @@ async function run() {
     })
 
     core.setOutput("matrix", jsonToMatrix(matrix));
-
+    core.info(`Output: ${jsonToMatrix(matrix)}`)
   } catch (error) {
     core.setFailed(error.message);
   }
