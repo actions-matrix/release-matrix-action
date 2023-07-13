7d3592190e4e80910332432850bb795d3ba41ad0
Socheat Sok <alex@socheat.net>
2023-07-13 16:48:23 +0700

    Add throw if no release found

--- index.js
+++ index.js
@@ -78,6 +78,10 @@ async function run() {
 
     const matrix = { version: [] }
 
+    if (releases.length === 0) {
+      throw new Error("No releases found.")
+    }
+
     releases.forEach(([ver]) => {
       matrix.version.push(ver)
     })
